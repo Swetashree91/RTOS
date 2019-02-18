ee27eec9d19389a6beee40ab75aa050e6f65865a
Swetashree91 <47529893+Swetashree91@users.noreply.github.com>
2019-02-19 01:04:15 +0530

    Update server.c

--- ASSIGNMENT-1/DatabaseEngine/server/server.c
+++ ASSIGNMENT-1/DatabaseEngine/server/server.c
@@ -1,67 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
-#include <stdio.h> 
-#include <unistd.h> 
-#include<stdlib.h>
-#include<sys/types.h>
-#include<sys/ipc.h>
-#include<sys/msg.h>
-#include<string.h>
+struct msg_queue { 
+    long mtype; 
+    char mtext[100]; 
+}buf;
 
+struct msg_queue sfunc(char file[], int no){
+    FILE *fp;
+    buf.mtype = 1 + no;
+    char string1[100];	
+    int length = strlen(file);
+	file[length-1] = file[length] ;
+    fp = fopen(file,"r");
+    if(fp == NULL){
+        strcpy(buf.mtext, "Data not found");
+        return buf;
+    }
+    char final[100] ;
+    while(fgets(string1, strlen(string1), fp) != NULL){
+        strcat(final, string1);
+    }   
 
+    fclose(fp);
+    strcpy(buf.mtext, final);
+    return buf;
+}
 
-struct mb_send{
-	long type;
-	char text[100];
-}msg1;
+int main()
+{
+    
+    struct msg_queue buf;
+    int msgid;
+    key_t key;
 
-struct mb_recv{
-	long typ;
-	char txt[100];
-}msg2;
+    if((key = ftok("man.txt", 'b')) == -1){
+        perror("ftok");
+        exit(1);
+    } 
 
-int main(void){
+    if ((msgid = msgget(key, 0644 | IPC_CREAT)) == -1) {
+        perror("msgget");
+        exit(1);
+    }
 
-	int mida,midb;
-	key_t key1,key2;
-	int i;
-	char text1[100];
-	char path[100];
-	FILE *fp;
-	
-	if((key1=ftok("123.txt",'b'))== -1){
-		perror("ftok\n");
-		exit(1);
-	}
-	
-	if((mida=msgget(key1,0644 | IPC_CREAT))==-1){
-		perror("msgget\n");
-		exit(1);
-	}
-	key2=ftok("man3.txt",'a');
-	midb=msgget(key2,0644 | IPC_CREAT);
-	msg2.typ = 1;
-		while(1){
-		msgrcv(mida,&msg1,sizeof(msg1),1,0);
-		printf("\nData received from Client A. : %s\n",msg1.text);
-			strcpy(path,msg1.text);
-			fp = fopen(path,"r");	
-			if(fp == NULL){
-				printf("\nfile not found.\n"); 
-				strcpy(msg2.txt,"\nfile not found.\n");
-				msgsnd(midb, &msg2, sizeof(msg2), 0);
-				exit(0);
-			}
-			else {
-				printf("/n file found.\n");
-				strcpy(msg2.txt,"\nfile found.\n");
-				msgsnd(midb, &msg2, sizeof(msg2), 0);
-				}
-		
-	
-			fclose(fp);
-			}
+    while(1) { 
+        
+        
+        msgrcv(msgid, &buf, sizeof buf.mtext, 1, 0);
+        
+        int len = strlen(buf.mtext);
+    
+	char no = buf.mtext[len];
+        printf("%s", buf.mtext);
+               
+        if(no == '1'){  
+            buf = sfunc(buf.mtext,1);
+        }
+        else if(no == '2'){
+            buf = sfunc(buf.mtext,2);    
+        }
+        else if(no == '3'){
+            buf = sfunc(buf.mtext,3);    
+        }
+        else if(no == '4'){
+            buf = sfunc(buf.mtext,4);    
+        }
+        else if(no == '5'){
+            buf = sfunc(buf.mtext,5);    
+        }        
+       
+        msgsnd(msgid, &buf, len, 0);
+            
+    }
 
-	msgctl(midb,IPC_RMID,NULL);
-	return 0;
-
- }
+    return 0;
+}
