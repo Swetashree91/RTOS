034dd4bd1b2d1e1a6374a229c108f9ce3b6782c9
Swetashree91 <47529893+Swetashree91@users.noreply.github.com>
2019-02-18 23:23:52 +0530

    Update clientA.c

--- ASSIGNMENT-1/EchoEngine/ClientA/clientA.c
+++ ASSIGNMENT-1/EchoEngine/ClientA/clientA.c
@@ -5,6 +5,7 @@
 #include<sys/ipc.h>
 #include<sys/msg.h>
 #include<string.h>
+#include<sys/time.h>
 
 
 
@@ -20,6 +21,7 @@ struct mb_recv1{
 
 int main(void){
 	
+	struct timeval t1,t2;	
 	int mid1,mid2;
 	key_t key1,key2;
 
@@ -31,12 +33,13 @@ int main(void){
 		perror("msgget\n");
 		exit(1);
 	}
+	mbuff1.type = 1;
 	printf("\nEnter a character: ");
 	
-	mbuff1.type = 1;
-	
-	gets(mbuff1.text);
-	
+	scanf("%s",mbuff1.text);
+		
+	gettimeofday(&t1,NULL);
+
 	msgsnd(mid1,&mbuff1,sizeof(mbuff1),0);	
 	
 	if(msgctl(mid1,IPC_RMID,NULL)==-1){
@@ -48,7 +51,9 @@ int main(void){
 	key2=ftok("/home/sweta/rtos/assgn1/Server/man2.txt",'a');
 	mid2=msgget(key2,0777 | IPC_CREAT);
 	msgrcv(mid2,&mbuf1,sizeof(mbuf1),1,0);	
+	gettimeofday(&t2,NULL);
 	printf("\nData received from server:  %s\n",mbuf1.txt);
+	printf("\n time by A:%lu",t2.tv_usec-t1.tv_usec);
 
 	return 0;
 }
